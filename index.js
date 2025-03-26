f9398eaa804cb49226fa34c6dd58b793b7bc3d30
Jaydenzel <jaydenzel.gitonga@student.moringaschool.com>
2025-03-26 12:01:02 +0300

    Scope Lab implemented

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    bestCustomer = "not bob"; // Implicitly global, as `var` isn't used
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+  }
+  const leastFavoriteCustomer = "some initial value";
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "new value"; // This will throw an error because constants cannot be reassigned
+  }
+   
